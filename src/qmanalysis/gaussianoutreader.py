598c2dbec84f29e8003504ae86b1da7c5584414e
Markus Walther <markus.walther@cookionista.com>
2025-08-27 05:17:29 +0000

    .

--- src/qmanalysis/gaussianoutreader.py
+++ src/qmanalysis/gaussianoutreader.py
@@ -143,9 +143,9 @@ class GaussianOutFile:
             split_block = archive_block.split('\\')
         else:
             split_block = []
-        # print("Archive split fields:")
-        # for idx, field in enumerate(split_block):
-        #    print(f"[{idx}]: {field}")
+        print("Archive split fields:")
+        for idx, field in enumerate(split_block):
+            print(f"[{idx}]: {field}")
 
         element_symbols = {'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh',
                            'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U'}
