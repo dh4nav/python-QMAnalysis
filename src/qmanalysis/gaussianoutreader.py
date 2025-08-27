808c898b6aeba6046f54aa7b5283c14960692a65
Markus Walther <markus.walther@cookionista.com>
2025-08-27 23:48:07 +0000

    .

--- src/qmanalysis/gaussianoutreader.py
+++ src/qmanalysis/gaussianoutreader.py
@@ -204,14 +204,15 @@ class GaussianOutFile:
             "dipole_z": dipole_z,
             "nimag": nimag
         }
-        print(
-            f"Saving row for {self.file_name}, {self.file_path}, {self.timestep_name}: {row}")
-        self.frame_data.dataframe.loc[(
-            self.file_name, self.file_path, self.timestep_name)] = row
+        # print(
+        #    f"Saving row for {self.file_name}, {self.file_path}, {self.timestep_name}: {row}")
+
         expected_cols = ["raw_data", "energy", "zero-point energy",
                          "file_comment", "charge", "multiplicity", "HF", "ZPE", "Thermal", "RMSD", "RMSF", "dipole", "dipole_x", "dipole_y", "dipole_z", "nimag"]
         self.frame_data.dataframe = self.frame_data.dataframe.reindex(
             columns=expected_cols)
+        self.frame_data.dataframe.loc[(
+            self.file_name, self.file_path, self.timestep_name)] = row
 
     @staticmethod
     def _atomic_number_to_symbol(num):
